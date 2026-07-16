495bfd03818a093551aab0a7b8b471dcc44ab42f
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-16 18:38:37 +0530

    Add basic arithmetic operations in operators.py

---
+++ operators.py
@@ -0,0 +1,9 @@
+a=10
+b=5
+print("Addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("modulous",a%b)
+print("exponent",a**b)
+print("floor division",a//b)
