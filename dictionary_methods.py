61443885d6984fa4f692aa335730d9c85689ccc4
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-22 16:54:58 +0530

    Modify student dictionary by updating age and deleting course
    Updated the student's age and removed the course.

---
+++ dictionary_methods.py
@@ -0,0 +1,11 @@
+student={
+  "name":"Pranjal",
+"age":18,
+  "course":"BScIT"
+}
+print("Original Dictionary:",student)
+# Add a new item 
+student["age"]=19
+# Delete an item
+del student["course"]
+print("Updated Dictionary",student)
