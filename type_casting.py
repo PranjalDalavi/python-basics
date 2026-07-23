2e70c986babbf461e59ca811dbfbf4d4f883faf3
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-23 18:59:37 +0530

    Add type casting examples in type_casting.py
    Demonstrate various type casting operations including string to int, float to int, and boolean conversions.

---
+++ type_casting.py
@@ -0,0 +1,31 @@
+
+
+age = "18"
+height = "170.5"
+
+# Convert string to integer
+age = int(age)
+
+# Convert string to float
+height = float(height)
+
+print("Age:", age)
+print("Height:", height)
+
+# Integer to float
+num = 25
+print("Integer to Float:", float(num))
+
+# Float to Integer
+pi = 3.14159
+print("Float to Integer:", int(pi))
+
+# Integer to String
+marks = 95
+print("Integer to String:", str(marks))
+
+# Boolean conversion
+print("Boolean of 1:", bool(1))
+print("Boolean of 0:", bool(0))
+print("Boolean of 'Python':", bool("Python"))
+print("Boolean of '':", bool(""))
