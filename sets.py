a1832242317fe61462894561368a597c8f49672a
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-23 12:35:53 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,16 @@
+# Creating a set
+
+fruits = {"Apple", "Banana", "Mango"}
+
+print("Original Set:", fruits)
+
+# Add an item
+fruits.add("Orange")
+print("After adding Orange:", fruits)
+
+# Remove an item
+fruits.remove("Banana")
+print("After removing Banana:", fruits)
+
+# Check if an item exists
+print("Is Mango in the set?", "Mango" in fruits)
